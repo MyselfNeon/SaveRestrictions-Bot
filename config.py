2ce7e337ed04142ce3338e0f29b9cb742a6de4cc
NeonAn <justasneon@gmail.com>
2025-09-18 22:19:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,20 @@
 import os
 
-# Bot token @Botfather
+# Bot Token
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-# Your API ID from my.telegram.org
+# Your API ID
 API_ID = int(os.environ.get("API_ID", ""))
 
-# Your API Hash from my.telegram.org
+# Your API Hash
 API_HASH = os.environ.get("API_HASH", "")
 
 # Your Owner / Admin Id For Broadcast 
 ADMINS = int(os.environ.get("ADMINS", "841851780"))
 
 # Your Mongodb Database Url
-# Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "MyselfNeon")
 
-# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
+# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then False
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
