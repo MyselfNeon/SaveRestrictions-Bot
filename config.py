1fb52526b340e1a70be486580be3d36d29f30334
NeonAn <justasneon@gmail.com>
2025-09-20 18:45:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,8 @@ import os
 # Bot Token
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-# Your API ID
+# Your API ID & Hash
 API_ID = int(os.environ.get("API_ID", ""))
-
-# Your API Hash
 API_HASH = os.environ.get("API_HASH", "")
 
 # Your Owner / Admin Id For Broadcast 
@@ -16,8 +14,8 @@ ADMINS = int(os.environ.get("ADMINS", "841851780"))
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "MyselfNeon")
 
+# Log Channel to Track New Users 
+LOG_CHANNEL = -1001889915480  # replace with your log channel id
+
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then False
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
-
-
-LOG_CHANNEL = -1001889915480   # replace with your log channel id
