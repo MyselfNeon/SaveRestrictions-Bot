0622bb8bf0009903f71c93e43ae1e5dff67aebba
NeonAn <justasneon@gmail.com>
2025-09-22 17:33:49 +0530

    Update start.py

--- Neon/start.py
+++ Neon/start.py
@@ -81,7 +81,7 @@ async def send_start(client: Client, message: Message):
     await client.send_message(
         chat_id=message.chat.id,
         text=(
-            f"<blockquote>**__‣ 👋 Hi {message.from_user.mention}__**</blockquote>\n"
+            f"<blockquote>**__Hellooo {message.from_user.mention}__ 😇**</blockquote>\n"
             "<blockquote>**__I’m Save Restricted Content Bot. I Can Help You Unlock And Save Restricted Posts From Telegram By Their Links.__**\n\n"
             "**__🔑 Please /login First — This Is Required For Downloading Content.__**\n\n"
             "**__📖 Use /help to learn more.__**</blockquote>"
